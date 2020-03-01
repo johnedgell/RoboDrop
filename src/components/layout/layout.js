a3e8f7951bf17d42946e2f471baff7c497cffdd6
John Edgell <johnedgell@gmail.com>
2020-03-01 19:46:56 +0000

    Renamed components/objects to components/lib and moved new folder to /src

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,10 +4,7 @@ import './style.css'
 export default function Layout(props) {
   return (
     <div className="view-panel-container">
-    	<div className="left-column">
-    		{props.left}
-    	</div>
-    	<div className="right-column">
+    	<div className="content">
     		<div className="output">{props.output}</div>
             <div className="lower">
                 {props.lower}
